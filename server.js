83a3448b3589749a41ef33e46e10f8abd55a50ff
Lsong <song940@gmail.com>
2021-09-18 17:47:13 +0800

    update

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+const http = require('http');
+const WeChat = require('wechat-lite');
+
+const app = new WeChat.Server('token', function (err, message) {
+  if (err) return console.error(err);
+  // console.log('Incoming', message);
+  switch (message.MsgType) {
+    case 'text':
+      return message.Content;
+    case 'link':
+      return message.Url;
+    case 'location':
+      return message.Label;
+    case 'event':
+      if (message.Event === 'subscribe') {
+        return "Hello, welcome to my wechat channel";
+      }
+      break;
+    case 'image':
+      return message.PicUrl;
+    default:
+      return message;
+  }
+});
+
+http.createServer(app).listen(3001);
